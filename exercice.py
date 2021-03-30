20d5f99978303d21d6bd5bf06b457fa8e2b49139
phhad <philippe.hadjis@polymtl.ca>
2021-03-30 09:31:14 -0400

    E

--- exercice.py
+++ exercice.py
@@ -2,12 +2,14 @@
 # -*- coding: utf-8 -*-
 
 # TODO: Importez vos modules ici
-
-
+import csv
+import pandas
 # TODO: Définissez vos fonctions ici
+def read(file):
+    pandas.read_csv(winequality_white.csv)
 
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-    pass
+    read(winequality_white.csv)
